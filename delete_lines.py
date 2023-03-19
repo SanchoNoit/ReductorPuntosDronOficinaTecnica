b97bc6f2b121fb573454f7266172b15fe7d29efa
SanchoNoit <von.sancho@gmail.com>
2023-03-19 09:11:04 +0100

    Cambio a GUI con TK y EASYGUI
    Cambias los inputs por prompt a un intento de abrir mediante GUI.

--- delete_lines.py
+++ delete_lines.py
@@ -1,11 +1,22 @@
 import sys
 
+# Importando paquetes de interfaz gráfica
+
+import easygui
+from Tkinter import Tk # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+
+#################################################################################
+
 if len(sys.argv) != 3:
     print("Usage: python3 program.py input.xyz output.xyz")
     sys.exit(1)
 
-input_file_name = sys.argv[1]
-output_file_name = sys.argv[2]
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+
+input_file_name = askopenfilename()
+
+output_file_name = easygui.enterbox("Nombre del archivo resultante: ")
 
 # Open the input file
 with open(input_file_name, 'r') as input_file:
