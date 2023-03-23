fa4d1278129290010cab937ee2f92aff707678bd
SanchoNoit <von.sancho@gmail.com>
2023-03-23 13:55:18 +0100

    Cambio a propuesta ChatGPT
    Propuesta CHATGPT para cambio Windowed

--- delete_lines.py
+++ delete_lines.py
@@ -1,19 +1,54 @@
-import sys
+import tkinter as tk
+from tkinter import filedialog
 
-if len(sys.argv) != 3:
-    print("Usage: python3 program.py input.xyz output.xyz")
-    sys.exit(1)
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Eliminar filas en archivo XYZ")
+        self.master.geometry("400x200")
+        
+        self.file_path = ""
+        self.skip_rate = 0
+        
+        self.file_label = tk.Label(master, text="Seleccione archivo XYZ:")
+        self.file_label.pack(pady=(20,10))
+        
+        self.file_button = tk.Button(master, text="Seleccionar archivo", command=self.open_file)
+        self.file_button.pack(pady=(0,20))
+        
+        self.skip_label = tk.Label(master, text="Ingrese número de filas a saltar de cada 100:")
+        self.skip_label.pack(pady=(0,10))
+        
+        self.skip_entry = tk.Entry(master)
+        self.skip_entry.pack()
+        
+        self.run_button = tk.Button(master, text="Ejecutar", command=self.process_file)
+        self.run_button.pack(pady=(20,0))
+        
+    def open_file(self):
+        self.file_path = filedialog.askopenfilename(defaultextension=".xyz", filetypes=[("XYZ files", "*.xyz")])
+        
+    def process_file(self):
+        if self.file_path == "":
+            tk.messagebox.showerror("Error", "Seleccione un archivo XYZ")
+        else:
+            try:
+                self.skip_rate = int(self.skip_entry.get())
+                with open(self.file_path, 'r') as input_file:
+                    with open("output.xyz", 'w') as output_file:
+                        line_count = 0
+                        skip_count = 0
+                        for line in input_file:
+                            if line_count % 100 >= self.skip_rate:
+                                output_file.write(line)
+                            else:
+                                skip_count += 1
+                            line_count += 1
+                        tk.messagebox.showinfo("Completado", f"Se eliminaron {skip_count} filas de cada 100 seleccionadas.")
+            except ValueError:
+                tk.messagebox.showerror("Error", "Ingrese un número entero válido para el salto de filas.")
+        
 
-input_file_name = sys.argv[1]
-output_file_name = sys.argv[2]
-
-# Open the input file
-with open(input_file_name, 'r') as input_file:
-    # Read all the lines from the input file
-    lines = input_file.readlines()
-
-# Open the output file
-with open(output_file_name, 'w') as output_file:
-    # Write every 40th line to the output file
-    for i in range(0, len(lines), 40):
-        output_file.write(lines[i])
+root = tk.Tk()
+app = App(root)
+root.mainloop()
